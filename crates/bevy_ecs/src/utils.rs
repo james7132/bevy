0d23a0b35590179fa37ff9f3fa9c158dff44d205
james7132 <contact@jamessliu.com>
2022-11-13 23:27:16 -0800

    Bounds check only in debug builds

--- crates/bevy_ecs/src/utils.rs
+++ crates/bevy_ecs/src/utils.rs
@@ -6,8 +6,11 @@ pub(crate) trait UnsafeVecExt<T> {
     ///
     /// This does not preserve ordering, but is O(1).
     ///
-    /// Unlike [`Vec::swap_remove`] , this does not panic and does not
-    /// do any bounds checking.
+    /// In release builds, this will not panic if `index` is invalid and
+    /// will not do any bounds checking.
+    /// 
+    /// # Panics
+    /// Will panic in debug builds if `index` is invalid.
     ///
     /// # Safety
     /// index must be less than the length of the [`Vec`].
@@ -18,6 +21,7 @@ impl<T> UnsafeVecExt<T> for Vec<T> {
     #[inline]
     unsafe fn swap_remove_unchecked(&mut self, index: usize) -> T {
         let len = self.len();
+        debug_assert!(index < len);
         let value = core::ptr::read(self.as_ptr().add(index));
         let base_ptr = self.as_mut_ptr();
         core::ptr::copy(base_ptr.add(len - 1), base_ptr.add(index), 1);
